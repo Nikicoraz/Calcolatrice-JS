4ee423333f5e0a88c681322ad0291c135c4eab3e
Nikicoraz <nicolacorato05@gmail.com>
2023-06-24 18:19:51 +0200

    Aggiunta virgola

--- main.ts
+++ main.ts
@@ -1,80 +1,80 @@
 const result = document.getElementById("result") as HTMLInputElement;
 let toClear = false;
 
-function clear(){
+function clear() {
     result.value = "";
 }
 
-function _evaluate(text: string, phase: string){
+function _evaluate(text: string, phase: string) {
     let ris = 0;
     let result: number[] = [];
     let splitted = text.split(phase);
-    if(phase == "/"){
+    if (phase == "/") {
         ris = parseFloat(splitted[0]);
-        for(let i = 1; i < splitted.length; i++){
+        for (let i = 1; i < splitted.length; i++) {
             ris /= parseFloat(splitted[i]);
         }
         return ris;
     }
     splitted.forEach(e => {
-        if (phase == "+"){
+        if (phase == "+") {
             result.push(_evaluate(e, "-"));
-        }else if(phase == "-"){
+        } else if (phase == "-") {
             result.push(_evaluate(e, "*"));
-        }else if(phase == "*"){
+        } else if (phase == "*") {
             result.push(_evaluate(e, "/"));
         }
     })
     ris = result[0];
-    if(phase == "+"){
-        for(let i = 1; i < result.length; i++){
+    if (phase == "+") {
+        for (let i = 1; i < result.length; i++) {
             ris += result[i];
         }
-    }else if(phase == "-"){
-        for(let i = 1; i < result.length; i++){
+    } else if (phase == "-") {
+        for (let i = 1; i < result.length; i++) {
             ris -= result[i];
         }
-    }else if(phase == "*"){
-        for(let i = 1; i < result.length; i++){
+    } else if (phase == "*") {
+        for (let i = 1; i < result.length; i++) {
             ris *= result[i];
         }
     }
     return ris;
 }
 
-function evaluate(text: string){
-    if(!text.match(/^([\d\.]+[\/*+-]?)+$/g)){
+function evaluate(text: string) {
+    if (!text.match(/^([\d\.]+[\/*+-]?)+$/g)) {
         result.value = "ERROR";
         return;
     }
     result.value = _evaluate(text, "+").toString();
 }
 
-function equalsPressed(){
+function equalsPressed() {
     toClear = true;
     evaluate(result.value);
 }
 
-result.addEventListener("keydown", (e) =>{
-    if(toClear){
+result.addEventListener("keydown", (e) => {
+    if (toClear) {
         toClear = false;
     }
-    if(e.key == "Enter"){
+    if (e.key == "Enter") {
         equalsPressed();
     }
 });
 
-Array.from(document.getElementsByTagName("button")).forEach(e =>{
-    e.addEventListener("click", (event) =>{
-        if(toClear){
+Array.from(document.getElementsByTagName("button")).forEach(e => {
+    e.addEventListener("click", (event) => {
+        if (toClear) {
             clear();
             toClear = false;
         }
-        switch(e.id){
+        switch (e.id) {
             case "clear":
                 clear();
                 break;
-            
+
             case "equals":
                 equalsPressed();
                 break;
@@ -85,5 +85,3 @@ Array.from(document.getElementsByTagName("button")).forEach(e =>{
         }
     });
 });
-
-// TODO crea pulsante virgola nell'HTML
